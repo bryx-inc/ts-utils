de37a2bba38a9aacab7a053995cbbe0678ef13fc
Tyler Holewinski <tyler.holewinski@bryx.com>
2022-12-28 13:21:14 -0600

    fix: formatting & add lint/format checks to `prepack`

--- src/condition.ts
+++ src/condition.ts
@@ -6,7 +6,7 @@ export function iff<T>(cond: boolean | undefined, v: T): T | undefined {
 /**
  * Expect a certain condition and, given that condition, return a value.
  * This often serves as a fill-in for `const v = maybe ? thing : (throw 'err')`, since that syntax is not supported.
- * 
+ *
  * @example
  * ```
  * type Result = {
@@ -15,15 +15,15 @@ export function iff<T>(cond: boolean | undefined, v: T): T | undefined {
  *  data?: string,
  *  ...
  * }
- * 
+ *
  * function getData() {
  *   const res: Result = ...
- * 
+ *
  *   if (res.isFetched)
  *     return expect(res.isSuccess, res.data, 'result failed!');
  * }
  * ```
- * 
+ *
  * @param assertation The control assertation to check.
  * @param then The value to return if the assertation passes.
  * @param err The Error or error message to throw of the assertation fails. If none is provided, a generic message will be thrown instead.
@@ -31,5 +31,5 @@ export function iff<T>(cond: boolean | undefined, v: T): T | undefined {
  */
 export function expect<T>(assertation: boolean, then: T, err?: Error | string): T {
     if (assertation) return then;
-    else throw err ?? `'expect' method found false assertation!`
+    else throw err ?? `'expect' method found false assertation!`;
 }
