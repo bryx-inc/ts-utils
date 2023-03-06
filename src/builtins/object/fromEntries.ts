bb38072b2a56f2b79b00b7141c249ab30a2cc31b
Tyler Holewinski <tyler.holewinski@bryx.com>
2023-03-06 11:13:08 -0500

    fix(builtins/object): wrap object constructer in declare global

--- src/builtins/object/fromEntries.ts
+++ src/builtins/object/fromEntries.ts
@@ -1,14 +1,17 @@
 import { Unarray } from "../../array";
+import { Narrow } from "../../types";
 
 type RecursiveWriteable<T> = { -readonly [P in keyof T]: RecursiveWriteable<T[P]> };
 
-type Cast<X, Y> = X extends Y ? X : Y;
+// type Cast<X, Y> = X extends Y ? X : Y;
 type FromEntries<T> = T extends [infer Key, any][]
-    ? { [K in Cast<Key, string>]: Extract<Unarray<T>, [K, any]>[1] }
+    ? { [K in Narrow<Key, string>]: Extract<Unarray<T>, [K, any]>[1] }
     : { [key in string]: any };
 
 type FromEntriesWithReadOnly<T> = FromEntries<RecursiveWriteable<T>>;
 
-export interface ObjectConstructor {
-    fromEntries<T>(obj: T): FromEntriesWithReadOnly<T>;
+declare global {
+    export interface ObjectConstructor {
+        fromEntries<T>(obj: T): FromEntriesWithReadOnly<T>;
+    }
 }
