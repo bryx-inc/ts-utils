1acfc8e4e864dc96d48926408ed4a833b9018c4d
Tyler Holewinski <tyler.holewinski@bryx.com>
2023-03-07 11:36:13 -0500

    chore(builtin): remove builtins

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -32,12 +32,7 @@ gulp.task("deploy", async () => {
     await $`git push origin main`;
 });
 
-gulp.task("build:lib", async () => {
-    await $`rm -rf dist/`;
-    await $`tsc`;
-    await $`cp -R src/builtins dist/`
-    await $`touch dist/builtins/object.js`;
-});
+gulp.task("build:lib", $`tsc`);
 
 gulp.task("check:lint", () => $`eslint .`);
 gulp.task("check:formatting", () => $`prettier -c src/*`);
