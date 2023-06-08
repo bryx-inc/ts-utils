eed7a0d7d8bfb04521438c58f23d8233d1dee5ad
Tyler Holewinski <tyler.holewinski@bryx.com>
2023-06-08 12:21:48 -0400

    Add `Result.from` factory (#43)

---
+++ .changeset/slow-beds-rush.md
@@ -0,0 +1,5 @@
+---
+"@bryx-inc/ts-utils": minor
+---
+
+Adds `Result.from`
--- src/result.test.ts
+++ src/result.test.ts
@@ -149,3 +149,94 @@ test("Result::toString", () => {
     expect(`${Result.Ok("foo")}`).toEqual('Ok("foo")');
     expect(`${Result.Err("foo")}`).toEqual('Err("foo")');
 });
+
+describe("Result.from", () => {
+    test("should return Result.Ok when the function executes successfully", () => {
+        const fn = () => 42;
+
+        const result = Result.from(fn);
+
+        expect(result.isOk()).toBe(true);
+        expect(result.ok()).toBe(42);
+    });
+
+    test("should return Result.Err when the function throws an error", () => {
+        const fn = () => {
+            throw new Error("Something went wrong");
+        };
+
+        const result = Result.from(fn);
+
+        expect(result.isErr()).toBe(true);
+        expect(result.err()).toBe("Error: Something went wrong");
+    });
+
+    test("should map the error using the provided mapErr function", () => {
+        const fn = () => {
+            throw new Error("Invalid argument");
+        };
+        const mapErr = (err: Error) => `CustomError: ${err.message}`;
+
+        const result = Result.from(() => {
+            throw new Error("Invalid argument");
+        }, mapErr);
+
+        expect(result.isErr()).toBe(true);
+        expect(result.err()).toBe("CustomError: Invalid argument");
+    });
+
+    test("Should throw an error if an invalid error type was thrown", () => {
+        const fn = () => {
+            throw new Error("Invalid argument");
+        };
+        const mapErr = (err: Error) => `CustomError: ${err.message}`;
+
+        const result = Result.from(() => {
+            throw "Invalid argument";
+        }, mapErr);
+
+        expect(result.isErr()).toBe(true);
+        expect(result.err()).toBe("CustomError: Invalid argument");
+    });
+
+    test("should return Result.Err with the default error message when an unknown error occurs", () => {
+        const result = Result.from(() => {
+            throw "Something went wrong";
+        });
+
+        expect(result.isErr()).toBe(true);
+        expect(result.err()).toBe("Error: Something went wrong");
+    });
+
+    test("should return Result.Err with the mapped value when an unknown error occurs and mapErr is provided", () => {
+        const fn = () => {
+            throw "Invalid argument";
+        };
+        const mapErr = (err: Error) => `Mapped Error: ${err.message}`;
+
+        const result = Result.from(fn, mapErr);
+
+        expect(result.isErr()).toBe(true);
+        expect(result.err()).toBe("Mapped Error: Invalid argument");
+    });
+
+    test("should return Result.Err with 'null' if a nullish value was thrown", () => {
+        const result = Result.from(() => {
+            throw null;
+        });
+
+        expect(result.isErr()).toBe(true);
+        expect(result.err()).toBe("Error: null");
+    });
+
+    test("should return Result.Err with the mapped value when a nullish value is thrown", () => {
+        const mapErr = (err: Error) => `Mapped Error: ${err.message}`;
+
+        const result = Result.from(() => {
+            throw null;
+        }, mapErr);
+
+        expect(result.isErr()).toBe(true);
+        expect(result.err()).toBe("Mapped Error: null");
+    });
+});
